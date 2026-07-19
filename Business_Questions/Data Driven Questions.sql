c987d7971eb3c88d0ff82b92017636cf5fe1e44d
Nomculo <khumalonomculo@gmail.com>
2026-07-19 17:16:48 +0000

    First questions

---
+++ Business_Questions/Data Driven Questions.sql
@@ -0,0 +1,15 @@
+-- Databricks notebook source
+#SUBSCRIBER ANALYSIS
+
+## Question 1
+How many subscribers does BrightTV have?
+
+## Question 2
+Which province has the highest number of subscribers?
+
+## Question 3
+What is the gender distribution of subscribers?
+
+## Qustion 4
+
+
